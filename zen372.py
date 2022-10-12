1e0332869c842f5a808cd7b91e8866c90b71f2e3
Panchito <daniel.carrillo90@uabc.edu.mx>
2022-10-11 20:13:42 -0700

    agregando base para modulos

---
+++ zen372.py
@@ -0,0 +1,19 @@
+import this
+#Comentario unitario
+"""
+  Comentario multilinea
+1 Linea
+2 Lineas
+3 ......
+
+Un archivo se crea para que sea mas rapido la ejecucion que el interpretado
+"""
+
+print("Latosa1: Nose")
+print("Latosa2: Mas vale pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme blablabla")
+print("MArco2: Fuga!!")
+print("Dereck: Fugax2 !!")
+
+barba = "Algo que sorpresa!!!!"
+
